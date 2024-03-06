1a86ca6ac6acd34fa8d2635360ef104e2e5bec2f
Aziz Cotrim <azizcotrim8@gmail.com>
2024-03-06 14:38:36 -0300

    Feito

---
+++ Teste1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Teste1 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Informe um número para verificar se ele pertence à sequência de Fibonacci:");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (VerificarFibonacci(numero)) {
+                Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+                Console.ReadLine();
+            } else {
+                Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+                Console.ReadLine();
+            }
+        }
+        static bool VerificarFibonacci(int num) {
+            int a = 0;
+            int b = 1;
+
+            while (a <= num) {
+                if (a == num) {
+                    return true;
+                }
+
+                int temp = a;
+                a = b;
+                b = temp + b;
+                Console.WriteLine($"{temp},{a},{b}");
+            }
+
+            return false;
+        }
+    }
+}
